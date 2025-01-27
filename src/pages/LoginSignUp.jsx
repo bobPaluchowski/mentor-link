b481938084d1fce8fa81c348771cd6759ad71aa6
sys-77 <systemseventy7@gmail.com>
2025-01-27 14:33:28 +0100

    login/sign up page

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,5 +1,31 @@
-function About() {
-  return <h1>About Page</h1>
-}
+import { supabase } from '../services/supabaseClient';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField } from '#mui/material';
 
-export default About;
+const LoginSignUp = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = async (email, password) => {
+    const { user, error } = await supabase.auth.signInWithPassword({ email, password });
+    if (user) navigate('/');
+    if (error) alert(error.message);
+  };
+
+  const handleGoogleLogin = async () => {
+    const { user, error } = await supabase.auth.signInWithOAuth({ provider: 'google' });
+    if (user) navigate('/');
+    if (error) alert(error.message);
+  };
+
+  return (
+  <div>
+      <h2>Login / Sign Up</h2>
+      <TextField label="Email" type="email" fullWidth />
+      <TextField label="Password" type="password" fullWidth />
+      <Button onClick={handleLogin}>Login</Button>
+      <Button onClick={handleGoogleLogin}>Login with Google</Button>
+    </div>
+  );
+};
+
+export default LoginSignUp;
